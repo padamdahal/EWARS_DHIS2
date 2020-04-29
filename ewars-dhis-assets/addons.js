9ef1ba88d1929c68bdcdf1b13ed8f8bb4989a0fb
padamdahal <dahal0514@gmail.com>
2020-04-29 12:32:21 +0545

    Update addons.js

--- ewars-dhis-assets/addons.js
+++ ewars-dhis-assets/addons.js
@@ -23,7 +23,7 @@ try{
 				
 				// Insert text input for nepali date
 				//var customDateField = $('<input type="text" class="customDatePicker form-control ng-pristine ng-untouched ng-invalid ng-invalid-required ng-invalid-date-validator ng-invalid-future-date-validator" placeholder="Select Nepali Date" style="position:relative;float:left;display:block;width:50% !important"/>');
-				var customDateField = $('<a class="nepali-calendar" href="" style="position:relative;float:left;"><img src="https://i.pinimg.com/236x/ca/65/54/ca655453eb79fe8db19601dfcf53ed95--vectors-calendar.jpg" style="width:50px;"/></a>');
+				var customDateField = $('<a class="nepali-calendar" style="position:relative;float:left;"><img src="https://i.pinimg.com/236x/ca/65/54/ca655453eb79fe8db19601dfcf53ed95--vectors-calendar.jpg" style="width:50px;"/></a>');
 				$(element[0]).after(customDateField);
 				//customDateField.appendTo(html);
 				customDateField.prev('input').css({"max-width":"50%","pointer-events":"none","color":"#ccc","position":"relative","float":"left"});
