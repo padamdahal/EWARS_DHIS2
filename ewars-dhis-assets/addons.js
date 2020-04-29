7ed234d705e7b3177d846ace670d9a07dd167b3a
padamdahal <dahal0514@gmail.com>
2020-04-29 11:07:03 +0545

    Update addons.js

--- ewars-dhis-assets/addons.js
+++ ewars-dhis-assets/addons.js
@@ -18,10 +18,10 @@ try{
 				
 				//element.css({"pointer-events":"none","border":"none","color":"#ccc","box-shadow":"none","position":"relative","float":"left","width":"50% !important"});
 				// Insert text input for nepali date
-				var customDateField = $('<input type="text" class="customDatePicker form-control ng-pristine ng-untouched ng-invalid ng-invalid-required ng-invalid-date-validator ng-invalid-future-date-validator" placeholder="Select Date" style="position:relative;float:left;display:block;width:50% !important"/>');
+				var customDateField = $('<input type="text" class="customDatePicker form-control ng-pristine ng-untouched ng-invalid ng-invalid-required ng-invalid-date-validator ng-invalid-future-date-validator" placeholder="Select Nepali Date" style="position:relative;float:left;display:block;width:50% !important"/>');
 				//element.before(customDateField);
 				customDateField.appendTo(html);
-				customDateField.previous('input').css({"pointer-events":"none","border":"none","color":"#ccc","box-shadow":"none","position":"relative","float":"left","width":"50% !important"});
+				customDateField.prev('input').css({"pointer-events":"none","border":"none","color":"#ccc","box-shadow":"none","position":"relative","float":"left","width":"50% !important"});
 						
 				// Attach nepali calendar in the input field
 				customDateField.calendarsPicker({
