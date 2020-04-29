9e1e44404d8b2fd7a158bc2270f0b09a7926ac04
padamdahal <dahal0514@gmail.com>
2020-04-29 12:39:33 +0545

    Update addons.js

--- ewars-dhis-assets/addons.js
+++ ewars-dhis-assets/addons.js
@@ -22,7 +22,7 @@ try{
 				var html = element.parent();
 				
 				// Insert text input for nepali date
-				var customDateField = $('<input type="text" class="customDatePicker form-control ng-pristine ng-untouched ng-invalid ng-invalid-required ng-invalid-date-validator ng-invalid-future-date-validator" placeholder="Select Nepali Date" style="position:relative;float:left;display:block;width:50px !important;background:url(https://i.pinimg.com/236x/ca/65/54/ca655453eb79fe8db19601dfcf53ed95--vectors-calendar.jpg ) no-repeat "/>');
+				var customDateField = $('<input type="text" class="customDatePicker form-control ng-pristine ng-untouched ng-invalid ng-invalid-required ng-invalid-date-validator ng-invalid-future-date-validator" placeholder="Select Nepali Date" style="position:relative;float:left;display:block;width:50px !important;background-image:url(https://i.pinimg.com/236x/ca/65/54/ca655453eb79fe8db19601dfcf53ed95--vectors-calendar.jpg) no-repeat "/>');
 				//var customDateField = $('<i class="nepali-calendar" style="position:relative;float:left;"><img src="https://i.pinimg.com/236x/ca/65/54/ca655453eb79fe8db19601dfcf53ed95--vectors-calendar.jpg" style="width:50px;"/></i>');
 				$(element[0]).after(customDateField);
 				//customDateField.appendTo(html);
